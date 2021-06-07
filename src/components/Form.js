fdd3cc42d13d6b8c074b1b50a10f921fa9a8d661
yilfri <yilfri97@hotmail.com>
2021-06-07 17:54:17 -0400

    Form component started

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,30 @@ import React from 'react';
 
 const Form = () => {
 	return (
-		<div>
-			<>
-				<h1>Form</h1>
-			</>
-		</div>
+		<form className="col-12">
+			<fieldset className="text-center">
+				<legend>Busca bebidas por Categoría o Ingredientes</legend>
+			</fieldset>
+
+			<div className="row mt-4">
+				<div className="col-md-4">
+					<input
+						type="text"
+						name="nombre"
+						className="form-control"
+						placeholder="Buscar por Ingredientes"
+					/>
+				</div>
+				<div className="col-md-4">
+					<select name="categoria" className="form-control">
+						<option value="">-- Selecciona Categoría --</option>
+					</select>
+				</div>
+				<div className="col-md-4">
+					<input type="submit" value="Buscar Bebidas" className="btn btn-block btn-primary" />
+				</div>
+			</div>
+		</form>
 	);
 };
 
